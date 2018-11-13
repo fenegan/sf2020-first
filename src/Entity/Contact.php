d19741df1f956f8ade8054a240a1ceef4bf30a92
Killian Vermersch <killian@idequanet.com>
2018-11-13 12:33:01 +0100

    Added Post Entiy and CRUD

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -18,16 +19,20 @@ class Contact
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Email(checkMX = true)
      */
     private $email;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
      */
     private $message;
 
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,29 +2,26 @@
 
 namespace App\Form;
 
-use App\Entity\Contact;
+use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ContactType extends AbstractType
+class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email', EmailType::class)
-            ->add('message')
-            ->add('send', SubmitType::class, ['label' => 'Send message'])
+            ->add('title')
+            ->add('content')
+            //->add('date')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Contact::class,
+            'data_class' => Post::class,
         ]);
     }
 }
